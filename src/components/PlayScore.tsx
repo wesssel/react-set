e87886c0fb6667c7fa31d1263d299ebffdd9c86e
Wessel  van der Pal <wesselvanderpal@gmail.com>
2020-03-18 20:12:43 +0100

    add timer

--- src/components/PlayScore.tsx
+++ src/components/PlayScore.tsx
@@ -6,17 +6,48 @@ interface Props {
   shuffle: () => void
 }
 
-export class PlayScore extends React.Component<Props, {}> {
+interface State {
+  secondsPlayed: number
+  showTimer: boolean
+}
+
+export class PlayScore extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
 
-    this.state = {}
+    this.state = {
+      secondsPlayed: 0,
+      showTimer: true
+    }
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({ secondsPlayed: this.state.secondsPlayed + 1 })
+    }, 1000)
+  }
+
+  get timeString(): string {
+    const date = new Date(0);
+    date.setSeconds(this.state.secondsPlayed);
+
+    return date.toISOString().substr(11, 8);
+  }
+
+  toggleTimer() {
+    this.setState({ showTimer: !this.state.showTimer })
   }
 
   render() {
     return (
       <div>
         <ul>
+          {this.state.showTimer
+            ? <li>
+              Timeplayed: {this.timeString}
+            </li>
+            : ''
+          }
           <li>
             Points: {this.props.points}
           </li>
@@ -25,6 +56,9 @@ export class PlayScore extends React.Component<Props, {}> {
           </li>
           <li>
             <button onClick={this.props.shuffle}>Shuffle!</button>
+          </li>
+          <li>
+            <button onClick={this.toggleTimer.bind(this)}>{this.state.showTimer ? 'Hide Timer' : 'Show Timer'}</button>
           </li>
         </ul>
       </div>
